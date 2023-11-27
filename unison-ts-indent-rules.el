920ff46ef7584c8fa97dc76121a38f3f4afaad8a
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2023-11-27 15:00:21 +0900

    feat: add indent rule for type signature

--- unison-ts-indent-rules.el
+++ unison-ts-indent-rules.el
@@ -22,7 +22,7 @@
 (defconst unison-ts-indent-rules--capture-parent-bol-pattern
   (regexp-opt '("term_definition" "type_declaration" "nested" "pattern"
                 "tuple_or_parenthesized" "literal_list" "tuple_pattern" "function_application"
-                "exp_if" "constructor" "delay_block")))
+                "exp_if" "constructor" "delay_block" "type_signature")))
 
 (defconst unison-ts-indent-rules--capture-node-eol-pattern
   (regexp-opt '("kw_then" "kw_else")))
