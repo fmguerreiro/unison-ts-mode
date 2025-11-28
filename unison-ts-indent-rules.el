3bf761fc8fd4de06ed55fa8ae68dc3eddd4b64c0
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2025-11-28 12:13:47 +0900

    Add GPL license boilerplate to all .el files.

--- unison-ts-indent-rules.el
+++ unison-ts-indent-rules.el
@@ -4,6 +4,19 @@
 
 ;; This file is not part of GNU Emacs.
 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; Defines tree-sitter indentation rules for Unison mode.
 ;;
@@ -98,4 +111,9 @@ Uses PARENT to find the containing term_definition and compute indent, or return
          (no-node parent 0))))
 
 (provide 'unison-ts-indent-rules)
+
+;; Local Variables:
+;; package-lint-main-file: "unison-ts-mode.el"
+;; End:
+
 ;;; unison-ts-indent-rules.el ends here
--- unison-ts-install.el
+++ unison-ts-install.el
@@ -4,6 +4,19 @@
 
 ;; This file is not part of GNU Emacs.
 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; Handles automatic installation of the tree-sitter grammar for Unison.
 
@@ -76,4 +89,9 @@ Returns t if grammar is available, nil otherwise."
     nil)))
 
 (provide 'unison-ts-install)
+
+;; Local Variables:
+;; package-lint-main-file: "unison-ts-mode.el"
+;; End:
+
 ;;; unison-ts-install.el ends here
